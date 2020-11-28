c5bb10b79fba999c652448ea985a047e90089353
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 02:20:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "."
   end
 
   def question?
-
+    self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!"
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[^a-zA-Z0-9, ]/)
+    split_sentence = split_sentence.reject { |e| e.empty? }
+    split_sentence.count
   end
 end
